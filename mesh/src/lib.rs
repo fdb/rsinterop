8baace2658031f08d64a7c07ba7428d3a4a19d69
Frederik De Bleser <frederik@debleser.be>
2017-06-27 12:36:43 +0200

    Cleanup mesh code.

--- mesh/src/lib.rs
+++ mesh/src/lib.rs
@@ -1,7 +1,3 @@
-use std::os::raw::c_char;
-use std::ffi::CString;
-use std::collections::HashMap;
-
 #[repr(C)]
 #[derive(Debug)]
 pub struct Vector3 {
@@ -28,32 +24,11 @@ impl Mesh {
 }
 
 #[no_mangle]
-pub fn my_add(a: f32, b: f32) -> f32 {
-    a + b
-}
-
-#[no_mangle]
-pub fn get_data() -> *mut c_char {
-    let mut data = HashMap::new();
-    data.insert("Alice", "send");
-    data.insert("Bob", "recieve");
-    data.insert("Carol", "intercept");
-
-    let descriptions = data.iter()
-        .map(|(p,a)| format!("{} likes to {} messages", p, a))
-        .collect::<Vec<_>>();
-
-    CString::new(descriptions.join(", "))
-        .unwrap()
-        .into_raw()
-}
-
-#[no_mangle]
-pub fn mesh_create_triangle() -> Box<Mesh> {
+pub extern "C" fn mesh_create_triangle() -> Box<Mesh> {
     Box::new(Mesh::triangle())
 }
 
 #[no_mangle]
-pub fn mesh_get_positions(mesh: &Mesh) -> &[Vector3] {
+pub extern "C"  fn mesh_get_positions(mesh: &Mesh) -> &[Vector3] {
     mesh.positions.as_slice()
 }
